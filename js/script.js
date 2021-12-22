1c16e60281eae03e38f963eac94d008a3152d582
Dinesh M <68711555+dinesh-raina@users.noreply.github.com>
2021-12-22 21:58:08 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ function validate()
 {
  var email=$("#email").val();
  var password=$("#password").val();
+ var email_val=/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+ var password_1=/([a-z].*[A-Z])|([A-Z].*[a-z])([0-9])+([!,%,&,@,#,$,^,*,?,_,~])/;
+ var password_2=/([0-9])/;
+ var password_3=/([!,%,&,@,#,$,^,*,?,_,~])/;
 
- var email_regex=/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
- var password_regex1=/([a-z].*[A-Z])|([A-Z].*[a-z])([0-9])+([!,%,&,@,#,$,^,*,?,_,~])/;
- var password_regex2=/([0-9])/;
- var password_regex3=/([!,%,&,@,#,$,^,*,?,_,~])/;
-
- if(email_regex.test(email)==false)
+ if(email_val.test(email)==false)
  {
-  alert("Please Enter Correct Email");
+
+//  alert("Please Enter Correct Email");
   return false;
  }
- else if(password.length<8 || password_regex1.test(password)==false || password_regex2.test(password)==false || password_regex3.test(password)==false)
+ else if(password.length<8 || password_1.test(password)==false || password_2.test(password)==false || password_3.test(password)==false)
  {
   alert("Please Enter Strong Password");
   return false;
@@ -23,3 +23,19 @@ function validate()
   return true;
  }
 }
+function showalert(message){
+ $("#emailVal").append(
+ " <div  class="alert alert-danger alert-dismissible fade show">" + "<strong>Error!</strong> Enter a valid Email Id <button type="button" class="btn-close" data-bs-dismiss="alert"></button>" + message + "</div>");
+}
+
+showalert("alert");
+$(document).ready(function){
+  $(".login").click(function){
+    if($("#email").val()=="dinesh@gmail.com" && $("#password").val()=="Dinesh123@"){
+    alert("success")
+    }else{
+    alert("failed")
+    }
+  });
+});
+
